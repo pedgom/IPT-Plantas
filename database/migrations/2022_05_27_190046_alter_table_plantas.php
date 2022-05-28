05581c454bfffede26fa8952e1330d883a26061f
Pedro Gomes <aluno21279@ipt.pt>
2022-05-28 19:15:41 +0100

    Alteração da ordem das migrations + Adição das tabelas M_N na migration alter_table

--- database/migrations/2022_05_27_190046_alter_table_plantas.php
+++ database/migrations/2022_05_27_190046_alter_table_plantas.php
@@ -15,11 +15,88 @@ return new class extends Migration
     {
         Schema::table('plantas', function (Blueprint $table) {
             //
+            $table->foreignId('categoria_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('origem_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
             $table->foreignId('altura_atributo_planta_id')
                 ->nullable()
                 ->constrained()
                 ->onDelete('set null');
         });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('diametro_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('densidade_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('luz_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('solo_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('ph_solo_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('agua_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('resistencia_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+
+
+
     }
 
     /**
@@ -29,10 +106,74 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::table('plantas', function (Blueprint $table) {
+            $table->foreignId('categoria_atributo_planta_id');
+            $table->dropColumn('categoria_atributo_planta_id');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            $table->foreignId('origem_atributo_planta_id');
+            $table->dropColumn('origem_atributo_planta_id');
+        });
 
         Schema::table('plantas', function (Blueprint $table) {
             $table->foreignId('altura_atributo_planta_id');
             $table->dropColumn('altura_atributo_planta_id');
         });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            $table->foreignId('diametro_atributo_planta_id');
+            $table->dropColumn('diametro_atributo_planta_id');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            $table->foreignId('densidade_atributo_planta_id');
+            $table->dropColumn('densidade_atributo_planta_id');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            $table->foreignId('luz_atributo_planta_id');
+            $table->dropColumn('luz_atributo_planta_id');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('solo_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('ph_solo_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('agua_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+        Schema::table('plantas', function (Blueprint $table) {
+            //
+            $table->foreignId('resistencia_atributo_planta_id')
+                ->nullable()
+                ->constrained()
+                ->onDelete('set null');
+        });
+
+
+
+
+
+
+
+
     }
 };
